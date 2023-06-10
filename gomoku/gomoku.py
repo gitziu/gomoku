3663a38b7a016fb9c8d21b8cfc2c3f1832346c5c
Ziu <ziu@staszic>
2023-06-10 19:57:51 +0200

    Lista gier

--- gomoku/gomoku.py
+++ gomoku/gomoku.py
@@ -7,17 +7,26 @@ app = Flask(__name__)
 # User {username: string, token: string}
 users = {} # Dict: username -> User
 tokens = {} # Dict: token -> User
+# Game {id: string, player1: string, player2: string, winner: string, board: string[][]}
+games = {} # Dict: game id -> Game
 
-def testData():
-    testUsers = [{"username": "tata", "token": "1111"},
-        {"username": "mama", "token": "2222"},
-        {"username": "Pawełek", "token": "3333"}]
-    for user in testUsers:
-        users[user["username"]] = user
-        tokens[user["token"]] = user
+gameId = 0
+
+def getGameId():
+    gameId += 1
+    return gameId
+
+def createBoard():
+    return [[""] * 19 for _ in range(19)]
 
 def error(msg):
     return jsonify({"error": msg})
+
+def auth():
+    token = request.args["token"]
+    if token not in tokens:
+        abort(401)
+    return tokens[token]
 
 @app.route("/")
 def main():
@@ -38,11 +47,36 @@ def login():
 
 @app.route("/users")
 def getUsers():
-    token = request.args["token"]
-    if token not in tokens:
-        return error("Zaloguj się")
-    username = tokens[token]["username"]
-    others = [u["username"] for u in users.values() if u["token"] != token]
-    return jsonify({"you": username, "users": others})
+    user = auth()
+    others = [u["username"] for u in users.values() if u != user]
+    return jsonify({"you": user["username"], "users": others})
+
+@app.route("/games", methods=["POST"])
+def startGame():
+    user = auth()
+    player2 = request.form["username"]
+    if player2 not in users:
+        return error("Nie ma takiego gracza")
+    game = {"id": getGameId(), "player1": user["username"], "player2": player2, "winner": "", "board": createBoard()}
+    games[game["id"]] = game
+    return jsonify({"gameId": game["id"], "games": games})
+
+@app.route("/games")
+def getGames():
+    auth()
+    return jsonify({"games": games})
+
+
+
+def testData():
+    testUsers = [{"username": "tata", "token": "1111"},
+        {"username": "mama", "token": "2222"},
+        {"username": "Pawełek", "token": "3333"}]
+    for user in testUsers:
+        users[user["username"]] = user
+        tokens[user["token"]] = user
+    testGames = [{"id": 1, "player1": "tata", "player2": "Pawłek", "winner": "", "board": createBoard()}]
+    for game in testGames:
+        games[game["id"]] = game
 
 testData()
