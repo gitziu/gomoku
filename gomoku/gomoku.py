26b12f239d123371cf401937aa2b6c1f81b918b0
Ziu <ziu@staszic>
2023-06-10 18:47:19 +0200

    Lista użytkowników

--- gomoku/gomoku.py
+++ gomoku/gomoku.py
@@ -1,10 +1,23 @@
 from flask import Flask, request, jsonify, abort, redirect, url_for
 import secrets
 
+# Flask tutorial: https://auth0.com/blog/developing-restful-apis-with-python-and-flask/
 app = Flask(__name__)
 
-users = {}
-tokens = {}
+# User {username: string, token: string}
+users = {} # Dict: username -> User
+tokens = {} # Dict: token -> User
+
+def testData():
+    testUsers = [{"username": "tata", "token": "1111"},
+        {"username": "mama", "token": "2222"},
+        {"username": "Pawełek", "token": "3333"}]
+    for user in testUsers:
+        users[user["username"]] = user
+        tokens[user["token"]] = user
+
+def error(msg):
+    return jsonify({"error": msg})
 
 @app.route("/")
 def main():
@@ -13,13 +26,23 @@ def main():
 @app.route("/login", methods=["POST"])
 def login():
     if "username" not in request.form:
-        return jsonify({"error": "Nie podano loginu"})
+        return error("Nie podano loginu")
     username = request.form["username"]
     if username in users:
-        return jsonify({"error": "Taki login jest już używany"})
+        return error("Taki login jest już używany")
     token = secrets.token_urlsafe()
     user = {"username": username, "token": token}
     users[username] = user
     tokens[token] = user
     return jsonify(user)
 
+@app.route("/users")
+def getUsers():
+    token = request.args["token"]
+    if token not in tokens:
+        return error("Zaloguj się")
+    username = tokens[token]["username"]
+    others = [u["username"] for u in users.values() if u["token"] != token]
+    return jsonify({"you": username, "users": others})
+
+testData()
